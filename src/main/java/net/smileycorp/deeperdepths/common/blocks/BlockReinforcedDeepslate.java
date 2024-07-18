827c45b026b3221535a2695dc5bad415d4baec28
Smileycorp <callum.j.longbottom@gmail.com>
2024-07-18 13:52:40 +0100

    add reinforced deepslate lang

--- src/main/java/net/smileycorp/deeperdepths/common/blocks/BlockReinforcedDeepslate.java
+++ src/main/java/net/smileycorp/deeperdepths/common/blocks/BlockReinforcedDeepslate.java
@@ -5,24 +5,18 @@ import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 
 public class BlockReinforcedDeepslate extends BlockDeeperDepths {
     
     public BlockReinforcedDeepslate() {
-        super("Reinforced_Deepslate", Material.ROCK, 55, 1200, 0);
+        super("Reinforced_Deepslate", Material.ROCK, 55, 1200, 4);
     }
     
     @Override
     public MapColor getMapColor(IBlockState state, IBlockAccess world, BlockPos pos) {
         return MapColor.GRAY;
-    }
-    
-    @Override
-    public boolean canHarvestBlock(IBlockAccess world, BlockPos pos, EntityPlayer player) {
-        return false;
     }
     
     @Override
@@ -32,7 +26,7 @@ public class BlockReinforcedDeepslate extends BlockDeeperDepths {
     
     @Override
     public boolean canEntityDestroy(IBlockState state, IBlockAccess world, BlockPos pos, Entity entity) {
-        return !(entity instanceof EntityPlayer);
+        return false;
     }
     
 }
