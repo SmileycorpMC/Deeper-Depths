35e1ba1eb21a5e0f17766d94b8e9da19392bc9f9
Sir Squidly <sir.squildy.le.third@gmail.com>
2024-07-23 00:07:13 -0700

    Added Oozing
    + Added Oozing Potion Effect
     ~ Very early, will require lots of polish, assets, ect
    + Added Potion of Oozing
    + Added Potion Registry in `DeeperDepthsPotions`

--- src/main/java/net/smileycorp/deeperdepths/common/potion/DeeperDepthsPotions.java
+++ src/main/java/net/smileycorp/deeperdepths/common/potion/DeeperDepthsPotions.java
@@ -1,10 +1,33 @@
 package net.smileycorp.deeperdepths.common.potion;
 
+import net.minecraft.entity.monster.EntitySlime;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.potion.PotionType;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.smileycorp.deeperdepths.common.Constants;
 
-public class DeeperDepthsPotions {
-    
+
+@Mod.EventBusSubscriber(modid = Constants.MODID)
+public class DeeperDepthsPotions
+{
     public static final Potion BAD_OMEN = null;
     public static final Potion TRIAL_OMEN = null;
-    
+    public static final Potion OOZING = new PotionOozing("oozing", true, 10092451, EntitySlime.class, 2, false);
+
+    public static final PotionType OOZING_POTION = new PotionType(Constants.MODID + "." + "oozing", new PotionEffect[] { new PotionEffect(OOZING, 3600, 0, false, true)} ).setRegistryName("oozing");
+
+    @SubscribeEvent
+    public static void onPotionRegister(RegistryEvent.Register<Potion> event)
+    {
+        event.getRegistry().register(DeeperDepthsPotions.OOZING);
+    }
+
+    @SubscribeEvent
+    public static void onPotionTypeRegister(RegistryEvent.Register<PotionType> event)
+    {
+        event.getRegistry().register(DeeperDepthsPotions.OOZING_POTION);
+    }
 }
---
+++ src/main/java/net/smileycorp/deeperdepths/common/potion/PotionOozing.java
@@ -0,0 +1,83 @@
+package net.smileycorp.deeperdepths.common.potion;
+
+import net.minecraft.entity.*;
+import net.minecraft.entity.monster.EntitySlime;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.Potion;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.smileycorp.deeperdepths.common.Constants;
+
+import java.util.Objects;
+
+/**
+ * A basic form of Oozing, still requires Particles, better spawning logic, better
+ * removal logic, and polish.
+ */
+public class PotionOozing extends Potion
+{
+    /** The entity to spawn. */
+    private final Class spawnEntity;
+    /** How many entities to spawn. */
+    private final int spawnQuantity;
+    /** If the entities should be initialized via `onInitialSpawn`. */
+    private final boolean doInitialSpawn;
+
+    protected PotionOozing(String nameIn, boolean isBadEffectIn, int liquidColorIn, Class spawnEntityIn, int spawnQuantityIn, boolean doInitialSpawnIn)
+    {
+        super(isBadEffectIn, liquidColorIn);
+        this.setPotionName("effect." + Constants.MODID + "." + nameIn);
+        this.setRegistryName(nameIn);
+        spawnEntity = spawnEntityIn;
+        spawnQuantity = spawnQuantityIn;
+        doInitialSpawn = doInitialSpawnIn;
+    }
+
+    @Override
+    public void performEffect(EntityLivingBase entity, int amplifier)
+    {
+        /* Particles will need to be fed through a Packet Handler. */
+        //entity.world.spawnParticle(EnumParticleTypes.SLIME, entity.posX, entity.posY, entity.posZ, 0, 0,0, new int[0]);
+
+        /* Currently hard-coded against Slimes, make configurable later. */
+        if (entity instanceof EntitySlime || !entity.isNonBoss()) entity.removePotionEffect(DeeperDepthsPotions.OOZING);
+
+        if(entity.isDead && !entity.world.isRemote)
+        {
+            for (int i = 0; i < spawnQuantity; i++)
+            {
+                //** Note that spawnEntity == EntitySlime *
+                Entity oozed = Objects.requireNonNull(EntityRegistry.getEntry(spawnEntity)).newInstance(entity.world);
+
+                if (oozed instanceof EntityLiving && doInitialSpawn) ((EntityLiving)oozed).onInitialSpawn(entity.world.getDifficultyForLocation(new BlockPos(entity)), (IEntityLivingData)null);
+
+                if (oozed instanceof EntitySlime) setSlimeStats(oozed, 1);
+
+                /* Currently is placed directly at the entity's death spot, implement 5x5x5 spawn area later. */
+                oozed.setLocationAndAngles(entity.posX, entity.posY, entity.posZ, entity.getRNG().nextFloat() * 360.0F, 0.0F);
+
+                entity.world.spawnEntity(oozed);
+            }
+        }
+    }
+
+    /** Slimes are annoyingly privatized, so we forcefully update slimes. */
+    public void setSlimeStats(Entity oozed, int size)
+    {
+        EntitySlime slime = ((EntitySlime)oozed);
+
+        NBTTagCompound nbt = new NBTTagCompound();
+        slime.writeEntityToNBT(nbt);
+        nbt.setInteger("Size", size);
+        slime.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue((double)(size * size));
+        slime.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue((double)(0.2F + 0.1F * (float)size));
+        slime.setHealth(slime.getMaxHealth());
+        //this.experienceValue = size;
+        slime.readEntityFromNBT(nbt);
+    }
+
+    /* Required so the effect actually runs. */
+    @Override
+    public boolean isReady(int duration, int amplifier)
+    { return true; }
+}
