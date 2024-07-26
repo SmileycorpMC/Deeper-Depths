1cb73f1476d5c45633fefa829561c99e1a00f351
Jboymercs <63374464+Jboymercs@users.noreply.github.com>
2024-07-25 22:58:39 -0500

    Stuff and Things
    added a spawn rate registry for the Bogged. Changed the shaft generation tiles

--- src/main/java/net/smileycorp/deeperdepths/common/entities/DeeperDepthsEntities.java
+++ src/main/java/net/smileycorp/deeperdepths/common/entities/DeeperDepthsEntities.java
@@ -1,7 +1,11 @@
 package net.smileycorp.deeperdepths.common.entities;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.smileycorp.deeperdepths.common.Constants;
@@ -16,6 +20,21 @@ public class DeeperDepthsEntities
         registerEntity("bogged", EntityBogged.class, ++id, 80, 9084018, 3231003);
     }
 
+    public static void registerEntitySpawns() {
+        spawnRate(EntityBogged.class, EnumCreatureType.MONSTER, 5, 1, 3, BiomeDictionary.Type.SWAMP);
+    }
+
     private static void registerEntity(String name, Class<? extends Entity> entity, int id, int range, int color1, int color2)
     { EntityRegistry.registerModEntity(new ResourceLocation(Constants.MODID, name), entity, Constants.MODID + "." + name, id, DeeperDepths.instance, range, 1, true, color1, color2); }
+
+
+    private static void spawnRate(Class<? extends EntityLiving> entityClass, EnumCreatureType creatureType, int weight, int min, int max, BiomeDictionary.Type biomesAllowed) {
+        for(Biome biome: BiomeDictionary.getBiomes(biomesAllowed)) {
+            if(biome != null && weight > 0) {
+                EntityRegistry.addSpawn(entityClass, weight, min, max, creatureType, biome);
+
+            }
+        }
+    }
+
 }
