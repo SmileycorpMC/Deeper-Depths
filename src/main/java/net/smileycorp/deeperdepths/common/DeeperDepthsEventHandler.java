17e0a2fdb9a8d1ee55c1e58f65ec3df76c865572
Smileycorp <callum.j.longbottom@gmail.com>
2024-08-03 20:32:03 +0100

    added lightning bolt copper stripping

--- src/main/java/net/smileycorp/deeperdepths/common/DeeperDepthsEventHandler.java
+++ src/main/java/net/smileycorp/deeperdepths/common/DeeperDepthsEventHandler.java
@@ -2,6 +2,7 @@ package net.smileycorp.deeperdepths.common;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemStack;
@@ -9,11 +10,14 @@ import net.minecraft.potion.Potion;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.PotionEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.smileycorp.deeperdepths.common.blocks.ICopperBlock;
 import net.smileycorp.deeperdepths.common.potion.DeeperDepthsPotions;
+
+import java.util.Random;
 
 public class DeeperDepthsEventHandler {
     
@@ -41,6 +45,24 @@ public class DeeperDepthsEventHandler {
         if (copper.interactRequiresSneak() &! entity.isSneaking()) return;
         if (stack.getItem() instanceof ItemAxe) copper.scrape(entity, world, stack, state, pos, event.getHand());
         if (stack.getItem() == Items.SLIME_BALL) copper.wax(entity, world, stack, state, pos, event.getHand());
+    }
+    
+    @SubscribeEvent
+    public void entityJoinWorld(EntityJoinWorldEvent event) {
+        World world = event.getWorld();
+        if (!(event.getEntity() instanceof EntityLightningBolt) || world.isRemote) return;
+        Random rand = world.rand;
+        BlockPos base = event.getEntity().getPosition().add(-1, -1, -1);
+        BlockPos.MutableBlockPos pos = new BlockPos.MutableBlockPos(base);
+        //copper stripping
+        for (int i = 0; i < 10; i++) {
+            pos.setPos(base.add(rand.nextInt(3), rand.nextInt(3), rand.nextInt(3)));
+            IBlockState state = world.getBlockState(pos);
+            if (!(state.getBlock() instanceof ICopperBlock)) return;
+            ICopperBlock copper = (ICopperBlock) state.getBlock();
+            if (copper.isWaxed(state)) return;
+            copper.scrape(world, state, pos);
+        }
     }
     
 }
