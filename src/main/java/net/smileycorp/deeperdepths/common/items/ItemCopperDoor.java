cea413cdf1cfe6fb481b8bc461f17a144415b518
Smileycorp <callum.j.longbottom@gmail.com>
2024-08-07 01:44:45 +0100

    fix copper door models

--- src/main/java/net/smileycorp/deeperdepths/common/items/ItemCopperDoor.java
+++ src/main/java/net/smileycorp/deeperdepths/common/items/ItemCopperDoor.java
@@ -1,9 +1,10 @@
 package net.smileycorp.deeperdepths.common.items;
 
 import net.minecraft.item.ItemDoor;
+import net.smileycorp.atlas.api.item.IMetaItem;
 import net.smileycorp.deeperdepths.common.blocks.BlockCopperDoor;
 
-public class ItemCopperDoor<T extends BlockCopperDoor> extends ItemDoor {
+public class ItemCopperDoor<T extends BlockCopperDoor> extends ItemDoor implements IMetaItem {
     
     public ItemCopperDoor(T block) {
         super(block);
