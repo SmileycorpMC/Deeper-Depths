1085941a2604d84a35e70ca647b0f8d351285885
Smileycorp <callum.j.longbottom@gmail.com>
2024-08-15 20:44:06 +0100

    fix mojanks item rarity parity

--- src/main/java/net/smileycorp/deeperdepths/common/items/ItemTrialKey.java
+++ src/main/java/net/smileycorp/deeperdepths/common/items/ItemTrialKey.java
@@ -1,8 +1,10 @@
 package net.smileycorp.deeperdepths.common.items;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
+import net.minecraftforge.common.IRarity;
 import net.smileycorp.deeperdepths.common.Constants;
 
 public class ItemTrialKey extends ItemDeeperDepths {
@@ -39,6 +41,14 @@ public class ItemTrialKey extends ItemDeeperDepths {
     
     public static boolean isOminous(int meta) {
         return meta == 1;
+    }
+    
+    //vanilla doesn't do this, but I'm kinda confused why it doesn't, all other ominous items are uncommon in vanilla
+    //why are the ominous keys common, it just doesn't make sense
+    //ngl I fully expect this to get changed in another snapshot after 24w33a
+    @Override
+    public IRarity getForgeRarity(ItemStack stack) {
+        return isOminous(stack) ? EnumRarity.UNCOMMON : super.getForgeRarity(stack);
     }
     
 }
