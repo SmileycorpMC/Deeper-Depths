3fe2e65b9a3a936e709da42ebb71b72b68a0184e
Smileycorp <callum.j.longbottom@gmail.com>
2024-09-05 21:43:52 +0100

    reenable raids support

--- src/main/java/net/smileycorp/deeperdepths/common/integration/RaidsIntegration.java
+++ src/main/java/net/smileycorp/deeperdepths/common/integration/RaidsIntegration.java
@@ -2,11 +2,21 @@ package net.smileycorp.deeperdepths.common.integration;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.EnumDifficulty;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.smileycorp.deeperdepths.common.potion.DeeperDepthsPotions;
+import net.smileycorp.raids.common.RaidsContent;
+import net.smileycorp.raids.common.raid.Raid;
+import net.smileycorp.raids.common.raid.RaidOmenTracker;
+import net.smileycorp.raids.common.raid.WorldDataRaids;
+import net.smileycorp.raids.config.RaidConfig;
 
 public class RaidsIntegration {
     
     public static void tickBadOmen(EntityPlayerMP player, int amplifier) {
-        /*World world = player.world;
+        World world = player.world;
         if (world.getDifficulty() == EnumDifficulty.PEACEFUL) return;
         if (!Raid.isVillage(world, player.getPosition())) return;
         Raid raid = WorldDataRaids.getData((WorldServer) world).getRaidAt(player.getPosition());
@@ -14,19 +24,19 @@ public class RaidsIntegration {
             if (RaidConfig.raidCenteredOnPlayer) RaidOmenTracker.setRaidStart(player);
             player.addPotionEffect(new PotionEffect(RaidsContent.RAID_OMEN, 600, amplifier));
             player.removePotionEffect(DeeperDepthsPotions.BAD_OMEN);
-        }*/
+        }
     }
     
     public static boolean hasBadOmen(EntityPlayer player) {
-        return /*player.isPotionActive(RaidsContent.RAID_OMEN)*/ false;
+        return player.isPotionActive(RaidsContent.RAID_OMEN);
     }
     
     public static int getBadOmenLevel(EntityPlayer player) {
-        return /*player.getActivePotionEffect(RaidsContent.RAID_OMEN).getAmplifier()*/ 0;
+        return player.getActivePotionEffect(RaidsContent.RAID_OMEN).getAmplifier();
     }
     
     public static void removeBadOmen(EntityPlayer player) {
-        //player.removePotionEffect(RaidsContent.RAID_OMEN);
+        player.removePotionEffect(RaidsContent.RAID_OMEN);
     }
     
 }
