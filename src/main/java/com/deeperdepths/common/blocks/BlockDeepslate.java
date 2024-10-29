6c57abd5a25d9761f5ed310e2a575348c03c8d29
Smileycorp <callum.j.longbottom@gmail.com>
2024-10-29 23:53:11 +0000

    fix deepslate not dropping cobbled deepslate when silk touch is not used

--- src/main/java/com/deeperdepths/common/blocks/BlockDeepslate.java
+++ src/main/java/com/deeperdepths/common/blocks/BlockDeepslate.java
@@ -3,10 +3,18 @@ package com.deeperdepths.common.blocks;
 import com.deeperdepths.common.Constants;
 import com.deeperdepths.common.DeeperDepths;
 import com.deeperdepths.common.DeeperDepthsSoundTypes;
+import com.deeperdepths.common.blocks.enums.EnumStoneType;
 import com.deeperdepths.config.BlockConfig;
 import net.minecraft.block.BlockRotatedPillar;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class BlockDeepslate extends BlockRotatedPillar implements IBlockProperties {
     
@@ -19,6 +27,21 @@ public class BlockDeepslate extends BlockRotatedPillar implements IBlockProperti
         setHarvestLevel("pickaxe", BlockConfig.deepslate.getHarvestLevel());
         setCreativeTab(DeeperDepths.CREATIVE_TAB);
         setSoundType(DeeperDepthsSoundTypes.DEEPSLATE);
+    }
+    
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return Item.getItemFromBlock(DeeperDepthsBlocks.STONE);
+    }
+    
+    @Override
+    public int damageDropped(IBlockState state) {
+        return EnumStoneType.COBBLED_DEEPSLATE.ordinal();
+    }
+    
+    @Override
+    public boolean canSilkHarvest(World world, BlockPos pos, IBlockState state, EntityPlayer player) {
+        return true;
     }
     
 }
