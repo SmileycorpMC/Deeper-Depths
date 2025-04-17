fc1e20f9e75a51bcf635335c7e78d35611caa9e0
Smileycorp <callum.j.longbottom@gmail.com>
2025-04-17 16:08:17 +0100

    basic spyglass functionality

--- src/main/java/com/deeperdepths/client/ClientProxy.java
+++ src/main/java/com/deeperdepths/client/ClientProxy.java
@@ -18,16 +18,23 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockDoor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.particle.Particle;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.block.statemap.StateMap;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
@@ -40,7 +47,9 @@ import java.awt.*;
 
 @Mod.EventBusSubscriber(value = Side.CLIENT, modid= Constants.MODID)
 public class ClientProxy extends CommonProxy {
-
+    
+    public static ResourceLocation SPYGLASS_TEXTURE = Constants.loc("textures/misc/spyglass_scope.png");
+    
     @Override
     public void handleAnimationPacket(int entityId, int index) {
         EntityPlayerSP player = Minecraft.getMinecraft().player;
@@ -127,7 +136,48 @@ public class ClientProxy extends CommonProxy {
         EntityPlayerSP player = mc.player;
         if (player.getItemInUseCount() <= 0) return;
         if (player.getActiveItemStack().getItem() != DeeperDepthsItems.SPYGLASS) return;
-        event.setFOV(0.1f);
+        event.setFOV(0.1f * event.getFOV());
+    }
+    
+    @SubscribeEvent
+    public static void postRenderOverlay(RenderGameOverlayEvent.Pre event) {
+        Minecraft mc = Minecraft.getMinecraft();
+        EntityPlayerSP player = mc.player;
+        if (player == null || event.getType() != RenderGameOverlayEvent.ElementType.ALL) return;
+        if (player.getItemInUseCount() <= 0) return;
+        if (player.getActiveItemStack().getItem() != DeeperDepthsItems.SPYGLASS) return;
+        ScaledResolution resolution = event.getResolution();
+        int height = resolution.getScaledHeight();
+        int width = (resolution.getScaledWidth() - height) / 2;
+        GlStateManager.enableBlend();
+        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        GlStateManager.pushMatrix();
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder bufferbuilder = tessellator.getBuffer();
+        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_COLOR);
+        bufferbuilder.pos(0, height, 0).color(0, 0, 0, 1f).endVertex();
+        bufferbuilder.pos(width, height, 0).color(0, 0, 0, 1f).endVertex();
+        bufferbuilder.pos(width, 0, 0).color(0, 0, 0, 1f).endVertex();
+        bufferbuilder.pos(0, 0, 0).color(0, 0, 0, 1f).endVertex();
+        tessellator.draw();
+        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_COLOR);
+        bufferbuilder.pos(width + height, height, 0).color(0, 0, 0, 1f).endVertex();
+        bufferbuilder.pos(resolution.getScaledWidth(), height, 0).color(0, 0, 0, 1f).endVertex();
+        bufferbuilder.pos(resolution.getScaledWidth(), 0, 0).color(0, 0, 0, 1f).endVertex();
+        bufferbuilder.pos(width + height, 0, 0).color(0, 0, 0, 1f).endVertex();
+        tessellator.draw();
+        mc.getTextureManager().bindTexture(SPYGLASS_TEXTURE);
+        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
+        bufferbuilder.pos(width, height, 0).tex(0, 1).endVertex();
+        bufferbuilder.pos(width + height, height, 0).tex(1, 1).endVertex();
+        bufferbuilder.pos(width + height, 0, 0).tex(1, 0).endVertex();
+        bufferbuilder.pos(width, 0, 0).tex(0, 0).endVertex();
+        tessellator.draw();
+        GlStateManager.popMatrix();
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        GlStateManager.disableBlend();
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
     }
     
 }
