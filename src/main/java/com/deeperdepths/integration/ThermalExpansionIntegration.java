945ce5e273cf8b28e36d3044e684211ddd6323f8
Smileycorp <callum.j.longbottom@gmail.com>
2025-09-08 22:54:47 +0100

    add thermal expansion recipes

---
+++ src/main/java/com/deeperdepths/integration/ThermalExpansionIntegration.java
@@ -0,0 +1,33 @@
+package com.deeperdepths.integration;
+
+import cofh.core.util.helpers.ItemHelper;
+import cofh.thermalexpansion.util.managers.machine.PulverizerManager;
+import cofh.thermalexpansion.util.managers.machine.SmelterManager;
+import com.deeperdepths.common.items.DeeperDepthsItems;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.smileycorp.atlas.api.item.ArmourSet;
+
+public class ThermalExpansionIntegration {
+
+    public static void registerRecipes() {
+        int smelterEnergy = SmelterManager.DEFAULT_ENERGY * 3 / 2;
+        ItemStack copperIngot = ItemHelper.getOre("ingotCopper");
+        for (Item item : DeeperDepthsItems.COPPER_TOOLS.getItems()) SmelterManager.addRecycleRecipe(smelterEnergy,
+                new ItemStack(item), copperIngot, 1);
+        SmelterManager.addRecycleRecipe(smelterEnergy, new ItemStack(DeeperDepthsItems.COPPER_ARMOR.getItem(ArmourSet.ArmorType.HELMET)),
+                copperIngot, 2);
+        SmelterManager.addRecycleRecipe(smelterEnergy, new ItemStack(DeeperDepthsItems.COPPER_ARMOR.getItem(ArmourSet.ArmorType.CHESTPLATE)),
+                copperIngot, 4);
+        SmelterManager.addRecycleRecipe(smelterEnergy, new ItemStack(DeeperDepthsItems.COPPER_ARMOR.getItem(ArmourSet.ArmorType.LEGGINGS)),
+                copperIngot, 3);
+        SmelterManager.addRecycleRecipe(smelterEnergy, new ItemStack(DeeperDepthsItems.COPPER_ARMOR.getItem(ArmourSet.ArmorType.BOOTS)),
+                copperIngot, 2);
+        SmelterManager.addRecycleRecipe(smelterEnergy, new ItemStack(DeeperDepthsItems.COPPER_ARMOR.getItem(ArmourSet.ArmorType.HORSE)),
+                copperIngot, 2, false);
+        PulverizerManager.addRecipe(75, ItemHelper.getOre("rodBreeze"), ItemHelper.getOre("dustBreeze", 6),
+                ItemHelper.getOre("dustSaltpeter", 1), 50);
+    }
+
+}
