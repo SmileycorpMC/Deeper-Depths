1b165735ac324551d6e712efe3e043f9b434c5ef
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-01-19 22:07:26 +0000

    remove unused maps from creative tab

--- src/main/java/com/deeperdepths/common/items/ItemExplorerMap.java
+++ src/main/java/com/deeperdepths/common/items/ItemExplorerMap.java
@@ -84,7 +84,7 @@ public class ItemExplorerMap extends ItemDeeperDepths {
     @Override
     public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items) {
         if (!isInCreativeTab(tab)) return;
-        for (int i = 0; i < 4; i++) items.add(new ItemStack(this, 1, i));
+        for (int i = 0; i < 1; i++) items.add(new ItemStack(this, 1, i));
     }
 
 
