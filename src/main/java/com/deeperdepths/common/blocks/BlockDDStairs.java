3cda2c7c8f5ff4c4aafb806e8fd37cb6d7accdd0
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-09-23 01:34:14 +0100

    fix stairs explosion resistance

--- src/main/java/com/deeperdepths/common/blocks/BlockDDStairs.java
+++ src/main/java/com/deeperdepths/common/blocks/BlockDDStairs.java
@@ -3,15 +3,29 @@ package com.deeperdepths.common.blocks;
 import com.deeperdepths.common.Constants;
 import com.deeperdepths.common.DeeperDepths;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Explosion;
+import net.minecraft.world.World;
 import net.smileycorp.atlas.api.block.BlockStairsBase;
 
+import javax.annotation.Nullable;
+
 public class BlockDDStairs extends BlockStairsBase implements IBlockProperties {
-    
+
+    private final IBlockState base;
+
     public BlockDDStairs(String name, IBlockState state) {
         super(name, state);
         setCreativeTab(DeeperDepths.CREATIVE_TAB);
         setSoundType(state.getBlock().getSoundType(state, null, null, null));
         setUnlocalizedName(Constants.name(name + "_stairs"));
+        base = state;
+    }
+
+    @Override
+    public float getExplosionResistance(World world, BlockPos pos, @Nullable Entity exploder, Explosion explosion) {
+        return base.getValue(BlockDDStone.VARIANT).getResistance() / 5f;
     }
     
 }
